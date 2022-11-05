f450f3b9b09b2b34679a0e8a2641d233b2b30db8
Md Rasedul Islam <86698952+rased100@users.noreply.github.com>
2022-11-05 11:27:27 +0600

    appLogo added

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import AppLogo from './Components/AppLogo';
 
+
 export default function App() {
   return (
     <View style={styles.container}>
@@ -15,7 +16,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#001E6C',
-    // backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
